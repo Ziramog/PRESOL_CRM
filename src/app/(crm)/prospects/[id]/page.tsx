2377e6ffc71a8a10b3b82428825bf4f1cda039ee
Ziramog <fullpowerok@gmail.com>
2026-09-14 08:36:16 -0400

    Fix: Resolve Next.js 15 params promise TS error that caused deployment failure

--- src/app/(crm)/prospects/[id]/page.tsx
+++ src/app/(crm)/prospects/[id]/page.tsx
@@ -131,8 +131,8 @@ export default async function ProspectDetailPage({ params }: { params: Promise<{
         
         {/* ROW 4: 2 columns (50/50 split) */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-2">
-          <InternalNotesAccordion comments={overview.comments} prospectId={params.id} />
-          <LinkedOpportunitiesAccordion opportunities={overview.opportunities} prospectId={params.id} />
+          <InternalNotesAccordion comments={overview.comments} prospectId={id} />
+          <LinkedOpportunitiesAccordion opportunities={overview.opportunities} prospectId={id} />
         </div>
         
       </div>
