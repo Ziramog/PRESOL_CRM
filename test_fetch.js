2edd9a3762b1a1a49a94a52745904d337e68027a
Ziramog <fullpowerok@gmail.com>
2026-09-15 20:43:26 -0400

    chore: remove scratch files to fix vercel build

--- test_fetch.js
+++
@@ -1,7 +0,0 @@
-async function test() {
-  const res = await fetch('http://localhost:3000/api/export-prospects');
-  console.log(res.status);
-  const blob = await res.blob();
-  console.log('Size:', blob.size);
-}
-test();
--- test_route.ts
+++
@@ -1,9 +0,0 @@
-import { GET } from './src/app/api/export-prospects/route.ts';
-async function test() {
-  const res = await GET();
-  const blob = await res.blob();
-  console.log('Blob size:', blob.size);
-  const text = await blob.text();
-  console.log('Text start:', text.substring(0, 50));
-}
-test();
